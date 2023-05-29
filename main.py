be31e78d88296622ab469d0d159477b175a2f7e1
yorichi123 <134946717+Erich675@users.noreply.github.com>
2023-05-29 18:37:42 +0300

    ЗАкончили день 1

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ class GameSprite(sprite.Sprite):
         self.rect.x = player_x
         self.rect.y = player_y
     def reset(self):
-        window blit(self.image, self.rect.x, self.rect.y)
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 
 class Player(GameSprite):
     def update_r(self):
@@ -25,11 +26,39 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y <win_height - 80:
             self.rect.y += self.speed
-            
-            
 
 back = (200, 255, 255)
 win_widhth = 600
 win_height = 500
-window = display.set_mode((win_widhtch, win_height))
+window = display.set_mode((win_widhth, win_height))
 window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racketr1 = Player("racet.png", 30,200,4,50,150)
+racketr2 = Player("racet.png", 520,200,4,50,150)
+ball = GameSprite("demon_head.png", 200,200,4,50,50)
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        racketr1.update_r()
+        racketr2.update_l()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        racketr1.reset()
+        racketr2.reset( )
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
