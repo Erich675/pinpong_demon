250ddf1a927be41708ce0b4396219010020b59cb
yorichi123 <134946717+Erich675@users.noreply.github.com>
2023-05-31 17:04:08 +0300

    Ljhf,jnfkb buhe

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from pygame import*
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed,  wight, height):
@@ -38,6 +39,12 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render("1 игрок проиграл", True, (180, 0, 0))
+lose2 = font.render("12 игрок проиграл", True, (180, 0, 0))
+
+
 racketr1 = Player("racet.png", 30,200,4,50,150)
 racketr2 = Player("racet.png", 520,200,4,50,150)
 ball = GameSprite("demon_head.png", 200,200,4,50,50)
@@ -55,6 +62,21 @@ while game:
         racketr2.update_l()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        if sprite.collide_rect(racketr1, ball) or sprite.collide_rect(racketr2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+            
+        if ball.rect.x > win_widhth:
+            finish = True
+            window.blit(lose2, (200,200))
+
 
         racketr1.reset()
         racketr2.reset( )
